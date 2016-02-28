7f4f038d583e205111236be81f8c7a277cb3611a
Kim Tore Jensen <kim@incendio.no>
2016-02-28 12:30:52 +0100

    Add setup.py file

---
+++ README.md
@@ -0,0 +1,12 @@
+# rsync file dump service
+
+## Development environment
+
+Set up a virtualenv and install the dependencies:
+
+```
+virtualenv deps
+source deps/bin/activate
+pip install -e .
+python setup.py develop
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "rsync",
+    version = "1.0",
+    packages = find_packages(),
+    install_requires = [
+        'django==1.9.2',
+        'django-tastypie',
+    ]
+)
