75ef77c23568c0279c8ad9a8e0d0963537952d9d
Kim Tore Jensen <kim@incendio.no>
2016-02-28 17:27:36 +0100

    Improve File admin interface

--- web/core/admin.py
+++ web/core/admin.py
@@ -4,4 +4,7 @@ import web.core.models
 
 @admin.register(web.core.models.File)
 class FileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['slug', 'file', 'author', 'created', 'expiry']
+    readonly_fields = ['id', 'slug']
+    ordering = ['-created']
+    fields = ['file', 'expiry']
