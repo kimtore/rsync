447f5d8892ead60a89f8b6b76ef5846f1ca4f597
Kim Tore Jensen <kim@incendio.no>
2016-10-03 21:37:21 +0200

    Upgrade dump script to Python3 and support the expiry parameter

--- bin/dump.py
+++ bin/dump.py
@@ -1,17 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
-# For use with Python versions older than 2.7.9, follow the instruction of this StackOverflow answer:
+# Installation and setup:
 #
-#   https://stackoverflow.com/questions/18578439/using-requests-with-tls-doesnt-give-sni-support/18579484#18579484
-#
-# In a nutshell, that means to execute the following commands as root:
-#
-#   apt-get install libffi-dev openssl-dev python-dev build-essential python-dateutil
-#   pip install urllib3 pyopenssl ndg-httpsclient pyasn1
-#
-# Also, you must install the pyperclip package, as root:
-#
-#   pip install pyperclip
+#   apt-get install python3-dateutil python3-pyperclip
 #
 # To use this script, insert the following into your ~/.netrc file and chmod it to 600:
 #
@@ -24,14 +15,21 @@
 #   /path/to/dump.py --url https://domain.com /path/to/file
 #
 
+HAS_PYPERCLIP = False
+
 import os
 import json
 import sys
 import requests
 import argparse
 import dateutil.parser
-import pyperclip
 import netrc
+
+try:
+    import pyperclip
+    HAS_PYPERCLIP = True
+except ImportError:
+    pass
 
 
 def valid_date(s):
@@ -51,9 +49,10 @@ def sizeof_fmt(num, suffix='B'):
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='File to upload')
+    parser.add_argument('--url', help='Base path to rsync web service', type=str, required=True)
     parser.add_argument('--expiry', help='Expiry date of file', type=valid_date)
-    parser.add_argument('--url', help='Base path to rsync web service', type=unicode, required=True)
-    parser.add_argument('--no-clipboard', help='Do not copy URL to clipboard', action='store_true')
+    if HAS_PYPERCLIP:
+        parser.add_argument('--no-clipboard', help='Do not copy URL to clipboard', action='store_true')
     return parser
 
 def get_authenticated_url(url):
@@ -74,11 +73,15 @@ if __name__ == '__main__':
 
     size = sizeof_fmt(os.stat(args.file).st_size)
 
-    files = {'file': open(args.file, 'r')}
+    files = {'file': open(args.file, 'rb')}
 
     sys.stderr.write("Uploading '%s' (%s)\n" % (args.file, size))
 
-    response = requests.post(url, files=files)
+    data = {}
+    if args.expiry:
+        data['expiry'] = args.expiry.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    response = requests.post(url, data, files=files)
 
     response.raise_for_status()
 
@@ -87,5 +90,5 @@ if __name__ == '__main__':
     sys.stdout.write("URL: %s\n" % content['url'])
     sys.stderr.write("Expires: %s\n" % content['expiry'])
 
-    if not args.no_clipboard:
+    if HAS_PYPERCLIP and not args.no_clipboard:
         pyperclip.copy(content['url'])
